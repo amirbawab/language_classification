287c2851cf7dd7f16f4afe1e1843361aedef6afa
Amir Bawab <amirbawab@gmail.com>
2017-10-06 12:00:40 -0400

    Added csv2csv

--- tools/cpp/csv2csv/src/csv2csv.cpp
+++ tools/cpp/csv2csv/src/csv2csv.cpp
@@ -3,9 +3,11 @@
 #include <getopt.h>
 #include <string.h>
 #include <algorithm>
+#include <fstream>
+#include <map>
 #include <csv.h>
 
-// Gloabl vars
+// Global vars
 std::string g_textFile;
 std::string g_langFile;
 std::string g_outputFile;
@@ -60,7 +62,11 @@ void initParams(int argc, char *argv[]) {
     }
 }
 
-
+struct utt {
+    int m_id;
+    int m_category;
+    std::string m_text;
+};
 
 int main(int argc, char** argv) {
 
@@ -73,5 +79,42 @@ int main(int argc, char** argv) {
         return 0;
     }
 
+    try{
+
+        // Prepare csv variables
+        int id;
+        int category;
+        std::string text;
+        std::map<int, struct utt> map;
+
+        // Load language information
+        io::CSVReader<2> langCSV(g_langFile);
+        langCSV.read_header(io::ignore_extra_column, "Id", "Category");
+        std::cout << ">> Loading: " << g_langFile << std::endl;
+        while(langCSV.read_row(id, category)){
+            map[id].m_id = id;
+            map[id].m_category = category;
+        }
+
+        // Load text information
+        std::cout << ">> Loading: " << g_textFile << std::endl;
+        io::CSVReader<2> textCSV(g_textFile);
+        textCSV.read_header(io::ignore_extra_column, "Id", "Text");
+        while(textCSV.read_row(id, text)) {
+            map[id].m_text = text;
+        }
+
+        // Generate new csv
+        std::cout << ">> Generating CSV: " << g_outputFile << std::endl;
+        std::ofstream outFile (g_outputFile);
+        outFile << "Id,Category,Text" << std::endl;
+        for(auto entry : map) {
+            outFile << entry.second.m_id << "," << entry.second.m_category << "," << entry.second.m_text << std::endl;
+        }
+        outFile.close();
+    } catch (io::error::can_not_open_file exception) {
+        std::cerr << "Error opening input file(s)" << std::endl;
+        return 1;
+    }
     return 0;
 }
