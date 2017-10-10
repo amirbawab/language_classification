ea88cf3eb4670ad9d6fcde1d5d0493af6fbda3f8
Amir Bawab <amirbawab@gmail.com>
2017-10-10 23:38:20 +0000

    Added threads

--- tools/python/scikit.py
+++ tools/python/scikit.py
@@ -7,24 +7,17 @@ import csv
 from sklearn.linear_model import SGDClassifier
 from sklearn.pipeline import Pipeline
 import numpy as np
+from threading import Thread
 
 # Set target/output
 categories = ['0','1','2','3','4']
 
+# Set cores
+cores = 8
+
 # Load files
 print(">> Loading data")
 twenty_train = sklearn.datasets.load_files('/home/amirbawab/data/')
-
-# Allow words with one characters
-count_vect = CountVectorizer(stop_words=None,token_pattern='(?u)\\b\\w\\w*\\b')
-
-# Train
-X_train_counts = count_vect.fit_transform(twenty_train.data)
-tf_transformer = TfidfTransformer(use_idf=False).fit(X_train_counts)
-
-tfidf_transformer = TfidfTransformer()
-X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
-clf = MultinomialNB().fit(X_train_tfidf, twenty_train.target)
 
 # Load new data
 print(">> Loading test data")
@@ -34,18 +27,42 @@ with open('/home/amirbawab/training_text.csv', newline='', encoding='utf-8') as 
      for row in reader:
          docs_new.append(row['Text'])
 
-# New
-print(">> Creating pipline")
-text_clf = Pipeline([('vect', count_vect),('tfidf', TfidfTransformer()),('clf', SGDClassifier(loss='hinge', penalty='l1',alpha=4e-5, random_state=42,max_iter=10, tol=None))])
-text_clf.fit(twenty_train.data, twenty_train.target)
-predicted = text_clf.predict(docs_new)
+# Pipline function
+def pipeline(itr,alpha):
+    print(">> Creating pipeline with iteration={}, alpha={}".format(itr, alpha))
+    text_clf = Pipeline([
+        ('vect', CountVectorizer(stop_words=None,token_pattern='(?u)\\b\\w\\w*\\b')),
+        ('tfidf', TfidfTransformer()),
+        ('clf', SGDClassifier(
+            loss='hinge', penalty='l1',alpha=alpha, random_state=42,max_iter=itr, tol=None, n_jobs=cores))])
 
-# Print to stdout
-c = 0
-filename = '/tmp/scikit.csv'
-print(">> Generating ouput:",filename)
-outputFile = open(filename,'w')
-outputFile.write('Id,Category')
-for doc, category in zip(docs_new, predicted):
-    outputFile.write('{},{}'.format(c, twenty_train.target_names[category]))
-    c += 1
+    print(">> Predicting ...")
+    text_clf.fit(twenty_train.data, twenty_train.target)
+    predicted = text_clf.predict(docs_new)
+
+    # Print to stdout
+    c = 0
+    filename = '/tmp/scikit-{}-{}.csv'.format(itr,alpha)
+    print(">> Generating ouput:",filename)
+    outputFile = open(filename,'w')
+    outputFile.write('Id,Category\n')
+    for doc, category in zip(docs_new, predicted):
+        outputFile.write('{},{}\n'.format(c, twenty_train.target_names[category]))
+        c += 1
+    outputFile.close()
+
+# New
+threads = []
+for itr in range(5,15,2):
+    for lalpha in range(1,10,3):
+        for ralpha in range(3,6):
+            alpha = float('{}e-{}'.format(lalpha, ralpha))
+            thread = Thread(target = pipeline, args = (itr,alpha,))
+            threads.append(thread)
+            thread.start()
+
+# Join threads
+for thread in threads:
+    thread.join()
+
+print(">> Done")
