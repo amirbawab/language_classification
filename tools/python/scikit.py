039bc04d9f54e2a95e7d0e51d8d044628c439a7f
Amir Bawab <amirbawab@gmail.com>
2017-10-10 15:49:26 -0400

    Enhanced configuration

--- tools/python/scikit.py
+++ tools/python/scikit.py
@@ -11,7 +11,7 @@ import numpy as np
 # Set target/output
 categories = ['0','1','2','3','4']
 
-print("Loading data ...")
+# Load files
 twenty_train = sklearn.datasets.load_files('/tmp/ml/data/')
 
 # Allow words with one characters
@@ -21,31 +21,26 @@ count_vect = CountVectorizer(stop_words=None,token_pattern='(?u)\\b\\w\\w*\\b')
 X_train_counts = count_vect.fit_transform(twenty_train.data)
 tf_transformer = TfidfTransformer(use_idf=False).fit(X_train_counts)
 
-#X_train_tf = tf_transformer.transform(X_train_counts)
 tfidf_transformer = TfidfTransformer()
 X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
 clf = MultinomialNB().fit(X_train_tfidf, twenty_train.target)
 
 # Load new data
 docs_new = []
-with open('/tmp/test_set_x.csv', newline='', encoding='utf-8') as csvfile:
+with open('/tmp/training_text.csv', newline='', encoding='utf-8') as csvfile:
      reader = csv.DictReader(csvfile)
      for row in reader:
          docs_new.append(row['Text'])
 
-# Predict
-#X_new_counts = count_vect.transform(docs_new)
-#X_new_tfidf = tfidf_transformer.transform(X_new_counts)
-#predicted = clf.predict(X_new_tfidf)
-
 # New
-text_clf = Pipeline([('vect', count_vect),('tfidf', TfidfTransformer()),('clf', SGDClassifier(loss='hinge', penalty='l2',alpha=1e-3, random_state=42,max_iter=5, tol=None))])
-text_clf.fit(twenty_train.data, twenty_train.target)
-predicted = text_clf.predict(docs_new)
+for i in range(5,20):
+    text_clf = Pipeline([('vect', count_vect),('tfidf', TfidfTransformer()),('clf', SGDClassifier(loss='hinge', penalty='l1',alpha=4e-5, random_state=42,max_iter=i, tol=None))])
+    text_clf.fit(twenty_train.data, twenty_train.target)
+    predicted = text_clf.predict(docs_new)
 
-# Print to stdout
-#c = 0
-#print('Id,Category')
-#for doc, category in zip(docs_new, predicted):
-#    print('{},{}'.format(c, twenty_train.target_names[category]))
-#    c += 1
+    # Print to stdout
+    c = 0
+    print('Id,Category')
+    for doc, category in zip(docs_new, predicted):
+        print('{},{}'.format(c, twenty_train.target_names[category]))
+        c += 1
