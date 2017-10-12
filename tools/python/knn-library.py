7c26e6836e6ffb21aa1e3302253d21d1335e981f
Deeksha Arya <deeksha.arya32@gmail.com>
2017-10-12 15:19:59 -0400

    KNN Library: Accept k as parameter

--- tools/python/knn-library.py
+++ tools/python/knn-library.py
@@ -9,6 +9,7 @@ import argparse
 parser = argparse.ArgumentParser()
 
 parser.add_argument("--file", "-f", type=str, required=True)
+parser.add_argument("--knn", "-k", type=str, required=True)
 args = parser.parse_args()
 
 X = []
@@ -22,7 +23,7 @@ with open(args.file, 'rb') as csvfile:
 vec = CountVectorizer(input='content',analyzer='char',decode_error='ignore',stop_words=None,ngram_range=(1,1))
 train_vectors = vec.fit_transform(X,y).toarray()
 
-neigh = KNeighborsClassifier(n_neighbors=3)
+neigh = KNeighborsClassifier(n_neighbors=int(args.knn[0]))
 neigh.fit(train_vectors, y)
 y_pred = neigh.predict(train_vectors)
-metrics.accuracy_score(y, y_pred)
+print(metrics.accuracy_score(y, y_pred))
