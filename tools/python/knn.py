d47a715b4aab0708deda2711840cc406ac741b1a
Deeksha Arya <deeksha.arya32@gmail.com>
2017-10-11 21:44:23 -0400

    KNN: scale to k and output results in file

--- tools/python/knn.py
+++ tools/python/knn.py
@@ -37,21 +37,25 @@ class KNN:
                         self.test_vectors[i][word] = 1
                 i += 1
 
-    def knn(self):
-        for test_row in self.test_vectors.keys():
-            closest_language = None
-            closest_distance = None
-            for lang in self.knn_results[test_row]:
-                print(self.knn_results[test_row])
-                if not closest_language or self.knn_results[test_row][lang] < closest_distance:
-                    closest_distance = self.knn_results[test_row][lang]
-                    closest_language = lang
-            print('Predicted language: '+closest_language)
+    def predict(self, outfile):
+        predictions = {}
+        f = open(outfile, 'w')
+        f.write('Id,Category\n')
+        for i in range(len(self.knn_results)):
+            predictions[i] = {}
+            for lang,v in self.knn_results[i].iteritems():
+                if lang in predictions[i]:
+                    predictions[i][lang] += 1
+                else:
+                    predictions[i][lang] = 1
+            language_predicted = sorted(predictions[i].iteritems(), key=lambda (k,v): (v,k), reverse=True)[0][0]
+            f.write(str(i)+','+str(language_predicted)+'\n')
+        f.close()
 
-
-    def knn_calc(self):
+    def knn_calc(self, k, outfile):
         for test_row in self.test_vectors.keys():
             self.knn_results[test_row] = {}
+            neighbours = {}
             for lang in self.languages.keys():
                 distance = 0
                 for word in self.test_vectors[test_row].keys():
@@ -59,21 +63,22 @@ class KNN:
                         distance += (self.test_vectors[test_row][word] - self.languages[lang][word]) ** 2
                     else:
                         distance += (self.test_vectors[test_row][word] - 0) ** 2
-                distance = sqrt(distance)
-                self.knn_results[test_row][lang] = distance
-        self.knn()
+                neighbours[lang] = sqrt(distance)
+            neighbours = sorted(neighbours.iteritems(), key=lambda (k,v): (v,k))
+            for i in range(int(k)):
+                self.knn_results[test_row][neighbours[i][0]] = neighbours[i][1]
+        self.predict(outfile)
 
     def run_knn(self):
         parser = argparse.ArgumentParser(description='KNN algorithm')
         parser.add_argument('-f','--text', nargs=1, help='CSV text and language file')
-        # parser.add_argument('-l','--lang', nargs=1, help='CSV language file')
+        parser.add_argument('-k','--knn', nargs=1, help='Number of nearest neighbours')
         parser.add_argument('-t','--test', nargs=1, help='CSV test file')
+        parser.add_argument('-o','--out', nargs=1, help='output CSV file')
         args = parser.parse_args()
-        # load_language_list(args.lang[0])
         self.vectorize_training_data(args.text[0])
         self.vectorize_test(args.test[0])
-        self.knn_calc()
-
+        self.knn_calc(args.knn[0], args.out[0])
 
 
 knn = KNN()
